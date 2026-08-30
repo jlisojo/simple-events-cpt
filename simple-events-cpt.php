a5297a143df1179977b24ef68b935cd722a41552
Josh <jlisojo@gmail.com>
2026-08-30 08:29:00 -0400

    Polish plugin release metadata

--- simple-events-cpt.php
+++ simple-events-cpt.php
@@ -5,7 +5,7 @@
  * Version: 1.0.0
  * Requires at least: 6.0
  * Requires PHP: 7.4
- * Author: Your Name
+ * Author: Josh
  * Text Domain: simple-events
  * License: GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
